a21fb45737309c59e6e525e6fc1d44ae6eb90448
isfonzar <icaro.isf@gmail.com>
2024-02-13 10:26:07 -0300

    start placeholder server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Hello, this is server-side rendered!</h1>")
 }
