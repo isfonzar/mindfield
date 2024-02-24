509115aebfd8ba507893abff87dfdf39136cb859
isfonzar <icaro.isf@gmail.com>
2024-02-24 17:04:47 -0300

    Create login page

--- server/main.go
+++ server/main.go
@@ -13,6 +13,7 @@ func main() {
 	// Serve files under the ./static/css directory under the /css prefix
 	http.Handle("/assets/css/", http.StripPrefix("/assets/css/", http.FileServer(http.Dir("./public/assets/css"))))
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", loginHandler)
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
@@ -22,6 +23,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	// Parse the HTML template
 	tmpl, err := template.ParseFiles("html/pages/home.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create an instance of `HomePage`
+	data := HomePage{}
+
+	// Execute the template, inserting the name into the placeholder
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	// Parse the HTML template
+	tmpl, err := template.ParseFiles("html/pages/login.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
